0d8bd68f5ddaf8d488da85d5433147dfdbff40e0
bluechry <bluechry@gmail.com>
2024-08-31 08:24:14 +0900

    update solution of reverse_bits.py

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -1,12 +1,28 @@
 from test_framework import generic_test
 
 
-def reverse_bits(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+# Solution 1
+def reverse_bits_1(x: int) -> int:
+    pass
+
+
+# Solution 2
+def reverse_bits_2(x: int) -> int:
+    pass
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('reverse_bits.py', 'reverse_bits.tsv',
-                                       reverse_bits))
+
+    # Test Solution 1
+    generic_test.generic_test_main(
+        'reverse_bits.py',
+        'reverse_bits.tsv',
+        reverse_bits_1
+    )
+
+    # Test Solution 2
+    generic_test.generic_test_main(
+        'reverse_bits.py',
+        'reverse_bits.tsv',
+        reverse_bits_2
+    )
