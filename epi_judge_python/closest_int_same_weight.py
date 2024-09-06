28b474f90d897c4d927ad9478568b7002c92a960
bluechry <bluechry@gmail.com>
2024-09-06 09:34:58 +0900

    Solved closest_int_same_weight.

--- epi_judge_python/closest_int_same_weight.py
+++ epi_judge_python/closest_int_same_weight.py
@@ -1,13 +1,26 @@
 from test_framework import generic_test
 
 
-def closest_int_same_bit_count(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+# Solution 3: Toggle the Rightmost 01 or 10 Bits in O(1) 
+def closest_int_same_bit_count_3(x: int) -> int:
+    # Find the righmost '...10...0' subsequence.
+    # 'pos' points the rightmost 1.
+    pos = x & ~(x - 1)
+
+    # If there is no '...10...0' subsequence,
+    # find the rightmost '...01...1' subsequnece.
+    # 'pos' points the rightmost 0.
+    if pos == 1:
+        pos = ~x & (x + 1)
+
+    # Toggle '10' or '01' bits
+    bitmask = pos | (pos >> 1)
+    return x ^ bitmask
 
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('closest_int_same_weight.py',
-                                       'closest_int_same_weight.tsv',
-                                       closest_int_same_bit_count))
+    generic_test.generic_test_main(
+        'closest_int_same_weight.py',
+        'closest_int_same_weight.tsv',
+        closest_int_same_bit_count_3
+    )
