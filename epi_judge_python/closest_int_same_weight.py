032b4790364cae33448a1774a44cdc13df8caa95
bluechry <bluechry@gmail.com>
2024-09-06 23:05:50 +0900

    Solve it again using a different approach.

--- epi_judge_python/closest_int_same_weight.py
+++ epi_judge_python/closest_int_same_weight.py
@@ -1,19 +1,27 @@
 from test_framework import generic_test
 
 
-# Solution 3: Toggle the Rightmost 01 or 10 Bits in O(1) 
-def closest_int_same_bit_count_3(x: int) -> int:
-    # Find the righmost '...10...0' subsequence.
-    # 'pos' points the rightmost 1.
-    pos = x & ~(x - 1)
+# Solution 1: Find and Toggle the Rightmost 01 or 10 Bits
+def closest_int_same_bit_count_1(x: int) -> int:
+    num_bits = 64
+    for i in range(num_bits - 1):
+        if (x >> i) & 1 != (x >> (i + 1)) & 1:
+            bitmask = (1 << i) | (1 << (i + 1))
+            return x ^ bitmask
 
-    # If there is no '...10...0' subsequence,
-    # find the rightmost '...01...1' subsequnece.
-    # 'pos' points the rightmost 0.
+    raise ValueError("All bits are 0 or 1.")
+
+
+# Solution 2: Toggle the Rightmost 01 or 10 Bits in O(1)
+def closest_int_same_bit_count_2(x: int) -> int:
+    # Find the rightmost '10' or '01' bit pair.
+    # Start by finding the rightmost set bit.
+    # If the LSB is 1, find the rightmost clear bit followed by a set bit.
+    pos = x & ~(x - 1)
     if pos == 1:
         pos = ~x & (x + 1)
 
-    # Toggle '10' or '01' bits
+    # Toggle the rightmost '10' or '01' bits.
     bitmask = pos | (pos >> 1)
     return x ^ bitmask
 
@@ -22,5 +30,11 @@ if __name__ == '__main__':
     generic_test.generic_test_main(
         'closest_int_same_weight.py',
         'closest_int_same_weight.tsv',
-        closest_int_same_bit_count_3
+        closest_int_same_bit_count_1
+    )
+
+    generic_test.generic_test_main(
+        'closest_int_same_weight.py',
+        'closest_int_same_weight.tsv',
+        closest_int_same_bit_count_2
     )
