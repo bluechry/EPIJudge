3190f0b52206bf20fbfb9b2634254b7666fe62c6
bluechry <bluechry@gmail.com>
2024-09-09 23:21:38 +0900

    Update problem

--- epi_judge_python/closest_int_same_weight.py
+++ epi_judge_python/closest_int_same_weight.py
@@ -1,13 +1,17 @@
 from test_framework import generic_test
 
 
-# Solution 1: Find and Toggle the Rightmost 01 or 10 Bits
 def closest_int_same_bit_count_1(x: int) -> int:
     pass
 
 
-# Solution 2: Toggle the Rightmost 01 or 10 Bits in O(1)
+# Solution 2: Find and Toggle the Rightmost 01 or 10 Bits
 def closest_int_same_bit_count_2(x: int) -> int:
+    pass
+
+
+# Solution 3: Toggle the Rightmost 01 or 10 Bits in O(1)
+def closest_int_same_bit_count_3(x: int) -> int:
     pass
 
 
@@ -22,4 +26,10 @@ if __name__ == '__main__':
         'closest_int_same_weight.py',
         'closest_int_same_weight.tsv',
         closest_int_same_bit_count_2
+    )
+
+    generic_test.generic_test_main(
+        'closest_int_same_weight.py',
+        'closest_int_same_weight.tsv',
+        closest_int_same_bit_count_3
     )
